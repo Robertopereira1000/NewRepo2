8861fabe0a358fee65871d1f15f8c4af1421071e
Roberto <Roberto@DESKTOP-M3LFS08>
2024-03-08 14:42:25 -0300

    Adicionar arquivos de projeto.

---
+++ whileApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whileApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 0;
+            while (cont < 100)  /* o programador pode por qual quer numero que sera repetido como
+                                 * no caso while (cont < 100) voce pode por qualquer valor que voce 
+                                 * queira repetir 
+                                 */
+            {
+                Console.WriteLine("Roberto Miguel");
+
+                cont++;
+            }
+        }
+    }
+}
